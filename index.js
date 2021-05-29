229bae16dbabe664c14e4b9df78728630521e680
serIvanich <serivan1108@gmail.com>
2021-05-29 18:54:04 +0300

    axios request

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const resultBlock = document.querySelector('#result')
 const pageNumberEl = document.querySelector('#page-number')
 const clickMeButton = document.querySelector('#click-me')
+const getTasksButton = document.querySelector('#get-tasks')
+
+// createTask('learn CSS')
+// deleteTasks("2b5c463a-e917-47ee-baac-2e55eef0ea02")
+
+updateTask("ea4c16f4-a3b6-4515-adfc-42ab883522e3", 'learn HTML')
 clickMeButton.addEventListener('click', () => {
 
     const promise = getImages(pageNumberEl.value)
-    promise.then(onDataReceived)
+    promise.then(onImagesReceived)
 })
 
-function onDataReceived(data) {
+getTasksButton.addEventListener('click', () => {
 
-            data.forEach(el => {
-                const img = document.createElement('img')
-                img.src = el.thumbnail
-                document.querySelector('#result').appendChild(img)
-            })
+    const promise = getTasks()
+    promise.then(onTasksReceived)
+})
 
+function onImagesReceived(data) {
 
+    data.forEach(el => {
+        const img = document.createElement('img')
+        img.src = el.thumbnail
+        document.querySelector('#result').appendChild(img)
+    })
 }
+
+function onTasksReceived(tasks) {
+    const result = document.querySelector('#tasks-result')
+    result.innerHTML = ''
+    tasks.forEach(task => {
+        const li = document.createElement('li')
+        li.innerHTML = task.title
+        li.dataset.id = task.id
+
+        result.appendChild(li)
+    })
+}
+
 
--- service.js
+++ service.js
@@ -1,6 +1,32 @@
 
 function getImages(pageNumber) {
-
-    const promise = $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`)
-    return promise
+    const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`)
+    return promise.then((response) => response.data)
 }
+
+function getTasks() {
+    const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=23456&count=15`)
+    return promise.then((response) => response.data)
+}
+
+function createTask(title) {
+    const promise = axios.post(`https://repetitora.net/api/JS/Tasks`, {
+        widgetId: 23456,
+        title: title
+    })
+    return promise.then((response) => response.data)
+}
+function updateTask(id, title) {
+    const promise = axios.put(`https://repetitora.net/api/JS/Tasks`, {
+        widgetId: 23456,
+        taskId: id,
+        title: title,
+
+    })
+    return promise.then((response) => response.data)
+}
+function deleteTasks(id) {
+    const promise = axios.delete(`https://repetitora.net/api/JS/Tasks?widgetId=23456&taskId=${id}`)
+    return promise.then((response) => response.data)
+}
+
